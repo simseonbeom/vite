ea7677529342810906dd69cc98b3fdbf2fee15c0
kindtiger <seonbeom2@gmail.com>
2023-12-28 17:10:44 +0900

    타입스크립트를 배워보자!

---
+++ pocketbase/pb_migrations/1703738013_updated_products.js
@@ -0,0 +1,76 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("z7tg64rhdc9hfsh")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "l1k2k4km",
+    "name": "price",
+    "type": "number",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "5qhyretb",
+    "name": "discount",
+    "type": "number",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("z7tg64rhdc9hfsh")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "l1k2k4km",
+    "name": "price",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "5qhyretb",
+    "name": "discount",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
--- src/pages/newPost/newPost.js
+++ src/pages/newPost/newPost.js
@@ -1,12 +1,88 @@
 
 
 import '/src/pages/newPost/newPost.css'
-import { setDocumentTitle, getNode } from '/src/lib/'
+import { setDocumentTitle, getNode, insertLast, clearContents } from '/src/lib/'
 import pb from '/src/api/pocketbase'
 
 
 
 setDocumentTitle('2.9cm / 상품 추가');
+
+
+
+function newPost(){
+
+  
+  const add = getNode('.add');
+  const imgField = getNode('#imgField');
+  const cancel = getNode('.cancel');
+
+  async function handleNewPost(){
+    
+    const formData = new FormData();
+
+    formData.append('brand',getNode('#brand').value);
+    formData.append('description',getNode('#description').value);
+    formData.append('price',getNode('#price').value);
+    formData.append('discount',getNode('#discount').value);
+    formData.append('photo',imgField.files[0]);
+
+    
+    try{
+      await pb.collection('products').create(formData);
+      location.href = '/src/pages/product/'
+    }
+    catch{
+      alert('상품 정보를 올바르게 입력해 주세요');
+    }
+    
+  }
+
+  function handleUpload(e){
+    const {files} = e.target;
+
+    const fileImages = Array.from(files).map((file)=>({
+      image:URL.createObjectURL(file),
+      label:file.name
+    }))
+
+    clearContents('.render')
+    
+    fileImages.forEach((item)=>{
+      const img = ` 
+        <img src="${item.image}" alt="${item.label}" />
+      ` 
+      insertLast('.render',img)
+    })
+ 
+  }
+
+  add.addEventListener('click',handleNewPost);
+  cancel.addEventListener('click',()=> history.back());
+  imgField.addEventListener('change',handleUpload)
+  
+  
+
+}
+
+
+
+newPost()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
